44cae0977ebaa5bbfb6464dd394d6480a43ec510
harry <dammykuye@gmail.com>
2022-04-10 02:45:42 +0100

    test setup

---
+++ api_package/setup_text.py
@@ -0,0 +1,21 @@
+import pytest
+from flask import Flask
+import routes
+from models import db
+from api_package import create_app
+from config import TestConfig
+
+
+@pytest.fixture
+def application():
+    app = create_app(TestConfig)
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(application):
+    return application.test_client()
