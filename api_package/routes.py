32ff57c1915c4e3968b628c369c6f72f55c99b48
harry <dammykuye@gmail.com>
2022-04-14 20:26:03 +0100

    change authentication input

--- api_package/routes.py
+++ api_package/routes.py
@@ -10,15 +10,15 @@ from flask import jsonify, make_response
 
 
 @auth.verify_password
-def verify_user(username_or_token, password):
-    if not username_or_token:
+def verify_user(email_or_token, password):
+    if not email_or_token:
         return False
     if not password:  # assumes that token was sent since password is empty
         g.token_used = True
         user = User()  # create an instance of the user class to verify token
-        g.user = user.verify_token(username_or_token)
+        g.user = user.verify_token(email_or_token)
         return True
-    user = User.query.filter_by(username=username_or_token).first()  # if username and password is sent
+    user = User.query.filter_by(email=email_or_token).first()  # if username and password is sent
     if not user or not user.verify_password(password):
         return False
     g.user = user
